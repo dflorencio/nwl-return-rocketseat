5295e44696b85cb57c140cefeae3894baf9105ad
Davi Florencio <davi_florencio@hotmal.com>
2022-05-04 17:39:03 -0300

    Steps  - Aula 2

--- web/src/components/CloseButton.tsx
+++ web/src/components/CloseButton.tsx
@@ -3,7 +3,7 @@ import { X } from 'phosphor-react'
 
 export function CloseButton(){
     return (
-        <Popover.Button className={"top-5 right-5 absolute text-zinc-400 houver:text-zinc-100"} title="Fechar Formulario">
+        <Popover.Button className={"top-5 right-5 absolute text-zinc-400 hover:text-zinc-100"} title="Fechar Formulario">
             <X  weight="bold" className='w-4 h-4 '/>
 
         </Popover.Button>
